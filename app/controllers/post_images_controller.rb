01857caa19657c617687abae75a372f2434f873b
EC2 Default User <ec2-user@ip-172-31-26-57.ap-northeast-1.compute.internal>
2023-03-19 17:34:33 +0000

    [Add]4 投稿一覧で表示する全ての画像をコントローラで取得

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,8 +1,8 @@
 class PostImagesController < ApplicationController
   def new
-    @post_images = PostImage.new
+    @post_image = PostImage.new
   end
-  
+
   # 投稿データの保存
   def create
     @post_image = PostImage.new(post_image_params)
@@ -12,14 +12,22 @@ class PostImagesController < ApplicationController
   end
 
   def index
+    @post_images = PostImage.all
   end
 
   def show
+    @post_image = PostImage.find(params[:id])
   end
-  
-  
+
+  def destroy
+    post_image = 削除するPostImageレコードを取得
+    post_image.削除
+    redirect_to PostImageの一覧ページへのパス
+  end
+
+
   private
-  
+
   def post_image_params
     params.require(:post_image).permit(:shop_name, :image, :caption)
   end
